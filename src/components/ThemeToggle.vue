66dd24f158024e9ae0398559465ad0003e38e132
David Visotsky <visotskydavid@gmail.com>
2026-02-18 16:01:02 -0500

    init

---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,9 @@
+<script></script>
+<template>
+  <button>
+    <svg stroke="yellow" fill="yellow">
+      <use xlink:href="/src/icons/sun.svg" fill="yellow" stroke="yellow" />
+    </svg>
+  </button>
+</template>
+<style scoped></style>
